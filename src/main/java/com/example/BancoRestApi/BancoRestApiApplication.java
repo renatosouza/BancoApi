c323277d9a98928989b896267ed568244975952e
Renato <renatosouzavv@gmail.com>
2019-11-03 14:23:18 -0300

    Commit inicial

---
+++ src/main/java/com/example/BancoRestApi/BancoRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.BancoRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoRestApiApplication.class, args);
+	}
+
+}
